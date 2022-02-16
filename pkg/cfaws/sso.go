1af5798b4b3c20d55599500ab64fa46dbbbbaa17
JoshuaWilkes <14214200+JoshuaWilkes@users.noreply.github.com>
2022-02-16 13:37:09 +0800

    Josh/cf 551 aws sso login flow should poll for token (#8)
    * poll for device authorization token
    
    * use color library
    
    * add colours and spinner
    
    * remove packages

--- pkg/cfaws/sso.go
+++ pkg/cfaws/sso.go
@@ -1,7 +1,6 @@
 package cfaws
 
 import (
-	"bufio"
 	"context"
 	"errors"
 	"fmt"
@@ -12,6 +11,7 @@ import (
 	"github.com/aws/aws-sdk-go-v2/service/sso"
 	ssotypes "github.com/aws/aws-sdk-go-v2/service/sso/types"
 	"github.com/aws/aws-sdk-go-v2/service/ssooidc"
+	ssooidctypes "github.com/aws/aws-sdk-go-v2/service/ssooidc/types"
 	"github.com/aws/aws-sdk-go-v2/service/sts"
 	"github.com/aws/aws-sdk-go-v2/service/sts/types"
 	"github.com/pkg/browser"
@@ -64,23 +64,19 @@ func (c *CFSharedConfig) SSOLogin(ctx context.Context) (aws.Credentials, error) 
 	}
 	// trigger OIDC login. open browser to login. close tab once login is done. press enter to continue
 	url := aws.ToString(deviceAuth.VerificationUriComplete)
-	fmt.Fprintf(os.Stdout, "If browser is not opened automatically, please open link:\n%v\n", url)
+	fmt.Fprintf(os.Stderr, "If browser is not opened automatically, please open link:\n%v\n", url)
 	err = browser.OpenURL(url)
 	if err != nil {
 		return aws.Credentials{}, err
 	}
-	fmt.Fprintln(os.Stdout, "Press ENTER key once login is done")
-	_ = bufio.NewScanner(os.Stdin).Scan()
-	// generate sso token
-	token, err := ssooidcClient.CreateToken(ctx, &ssooidc.CreateTokenInput{
-		ClientId:     register.ClientId,
-		ClientSecret: register.ClientSecret,
-		DeviceCode:   deviceAuth.DeviceCode,
-		GrantType:    aws.String("urn:ietf:params:oauth:grant-type:device_code"),
-	})
+
+	fmt.Fprintln(os.Stderr, "\nAwaiting authentication in the browser...")
+	token, err := PollToken(ctx, ssooidcClient, *register.ClientSecret, *register.ClientId, *deviceAuth.DeviceCode, PollingConfig{CheckInterval: time.Second * 2, TimeoutAfter: time.Minute * 2})
 	if err != nil {
 		return aws.Credentials{}, err
 	}
+
+	fmt.Fprintln(os.Stderr, "Successfully authenticated")
 	// create sso client
 	ssoClient := sso.NewFromConfig(cfg)
 	res, err := ssoClient.GetRoleCredentials(ctx, &sso.GetRoleCredentialsInput{AccessToken: token.AccessToken, AccountId: &rootProfile.RawConfig.SSOAccountID, RoleName: &rootProfile.RawConfig.SSORoleName})
@@ -102,46 +98,12 @@ func (c *CFSharedConfig) SSOLogin(ctx context.Context) (aws.Credentials, error) 
 			if err != nil {
 				return aws.Credentials{}, err
 			}
-			fmt.Fprintf(os.Stderr, "\n\033[32mAssumed role for : %s\033[0m\n", p.Name)
+			fmt.Fprintf(os.Stderr, "\033[32m\nAssumed role for: %s\033[0m\n", p.Name)
 			credProvider = &CredProv{TypeCredsToAwsCreds(*stsRes.Credentials)}
 
 		}
 	}
 	return credProvider.Credentials, nil
-
-	// // This may be unnecessary, but it reveals the full list of accounts per ssoClient
-	// fmt.Println("Fetching list of all accounts for user")
-	// accountPaginator := sso.NewListAccountsPaginator(ssoClient, &sso.ListAccountsInput{
-	// 	AccessToken: token.AccessToken,
-	// })
-	// for accountPaginator.HasMorePages() {
-	// 	x, err := accountPaginator.NextPage(ctx)
-	// 	if err != nil {
-	// 		return err
-	// 	}
-	// 	for _, y := range x.AccountList {
-	// 		fmt.Println("-------------------------------------------------------")
-	// 		fmt.Printf("\nAccount ID: %v\nName: %v\nEmail: %v\n", aws.ToString(y.AccountId), aws.ToString(y.AccountName), aws.ToString(y.EmailAddress))
-
-	// 		// list roles for a given account [ONLY provided for better example coverage]
-	// 		fmt.Printf("\n\nFetching roles of account %v for user\n", aws.ToString(y.AccountId))
-	// 		rolePaginator := sso.NewListAccountRolesPaginator(ssoClient, &sso.ListAccountRolesInput{
-	// 			AccessToken: token.AccessToken,
-	// 			AccountId:   y.AccountId,
-	// 		})
-	// 		for rolePaginator.HasMorePages() {
-	// 			z, err := rolePaginator.NextPage(ctx)
-	// 			if err != nil {
-	// 				return err
-	// 			}
-	// 			for _, p := range z.RoleList {
-	// 				fmt.Printf("Account ID: %v Role Name: %v\n", aws.ToString(p.AccountId), aws.ToString(p.RoleName))
-	// 			}
-	// 		}
-
-	// 	}
-	// }
-	// fmt.Println("-------------------------------------------------------")
 
 }
 
@@ -157,3 +119,69 @@ type CredProv struct{ aws.Credentials }
 func (c *CredProv) Retrieve(ctx context.Context) (aws.Credentials, error) {
 	return c.Credentials, nil
 }
+
+var ErrTimeout error = errors.New("polling for device authorization token timed out")
+
+type PollingConfig struct {
+	CheckInterval time.Duration
+	TimeoutAfter  time.Duration
+}
+
+//PollToken will poll for a token and return it once the authentication/authorization flow has been completed in the browser
+func PollToken(ctx context.Context, c *ssooidc.Client, clientSecret string, clientID string, deviceCode string, cfg PollingConfig) (*ssooidc.CreateTokenOutput, error) {
+	start := time.Now()
+	for {
+		time.Sleep(cfg.CheckInterval)
+
+		token, err := c.CreateToken(ctx, &ssooidc.CreateTokenInput{
+			ClientId:     &clientID,
+			ClientSecret: &clientSecret,
+			DeviceCode:   &deviceCode,
+			GrantType:    aws.String("urn:ietf:params:oauth:grant-type:device_code"),
+		})
+		var pendingAuth *ssooidctypes.AuthorizationPendingException
+		if err == nil {
+			return token, nil
+		} else if !errors.As(err, &pendingAuth) {
+			return nil, err
+		}
+
+		if time.Now().After(start.Add(cfg.TimeoutAfter)) {
+			return nil, ErrTimeout
+		}
+	}
+}
+
+// // This may be unnecessary, but it reveals the full list of accounts per ssoClient
+// fmt.Println("Fetching list of all accounts for user")
+// accountPaginator := sso.NewListAccountsPaginator(ssoClient, &sso.ListAccountsInput{
+// 	AccessToken: token.AccessToken,
+// })
+// for accountPaginator.HasMorePages() {
+// 	x, err := accountPaginator.NextPage(ctx)
+// 	if err != nil {
+// 		return err
+// 	}
+// 	for _, y := range x.AccountList {
+// 		fmt.Println("-------------------------------------------------------")
+// 		fmt.Printf("\nAccount ID: %v\nName: %v\nEmail: %v\n", aws.ToString(y.AccountId), aws.ToString(y.AccountName), aws.ToString(y.EmailAddress))
+
+// 		// list roles for a given account [ONLY provided for better example coverage]
+// 		fmt.Printf("\n\nFetching roles of account %v for user\n", aws.ToString(y.AccountId))
+// 		rolePaginator := sso.NewListAccountRolesPaginator(ssoClient, &sso.ListAccountRolesInput{
+// 			AccessToken: token.AccessToken,
+// 			AccountId:   y.AccountId,
+// 		})
+// 		for rolePaginator.HasMorePages() {
+// 			z, err := rolePaginator.NextPage(ctx)
+// 			if err != nil {
+// 				return err
+// 			}
+// 			for _, p := range z.RoleList {
+// 				fmt.Printf("Account ID: %v Role Name: %v\n", aws.ToString(p.AccountId), aws.ToString(p.RoleName))
+// 			}
+// 		}
+
+// 	}
+// }
+// fmt.Println("-------------------------------------------------------")
