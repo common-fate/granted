df4016b85eb9116c8d2958b79e3141031dd3939e
meyerjrr <33018450+meyerjrr@users.noreply.github.com>
2022-02-23 07:30:32 +0800

    update fish script (#33)

--- scripts/assume.fish
+++ scripts/assume.fish
@@ -6,7 +6,7 @@ set -gx GRANTED_ALIAS_CONFIGURED "true"
 #GRANTED_n - the data from granted
 set GRANTED_OUTPUT (assumego $argv)
 set GRANTED_STATUS $status
-echo $GRANTED_OUTPUT | read GRANTED_FLAG GRANTED_1 GRANTED_2 GRANTED_3
+echo $GRANTED_OUTPUT | read GRANTED_FLAG GRANTED_1 GRANTED_2 GRANTED_3 GRANTED_4
 
 
 # remove carriage return
@@ -19,6 +19,7 @@ else if test "$GRANTED_FLAG" = "GrantedAssume"
   set -e AWS_ACCESS_KEY_ID
   set -e AWS_SECRET_ACCESS_KEY
   set -e AWS_SESSION_TOKEN
+  set -e GRANTED_AWS_ROLE_PROFILE
 
   set -gx GRANTED_COMMAND $argv
   if test "$GRANTED_1" != "None"
@@ -30,6 +31,9 @@ else if test "$GRANTED_FLAG" = "GrantedAssume"
   if test "$GRANTED_3" != "None"
     set -gx AWS_SESSION_TOKEN $GRANTED_3
   end
+  if test "$GRANTED_4" != "None"
+    set -gx GRANTED_AWS_ROLE_PROFILE $GRANTED_4
+  end
 
   if contains -- -s $argv
     if test "$GRANTED_1" != "None"
@@ -40,6 +44,9 @@ else if test "$GRANTED_FLAG" = "GrantedAssume"
     end
     if test "$GRANTED_3" != "None"
       echo set -gx AWS_SESSION_TOKEN $GRANTED_3
+    end
+    if test "$GRANTED_4" != "None"
+      echo set -gx GRANTED_AWS_ROLE_PROFILE $GRANTED_4
     end
   end
 end
