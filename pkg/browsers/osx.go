b2f9c841bbfb2c6a83bd4cf9f0cfc4fa9c470874
Joshua Wilkes <14214200+JoshuaWilkes@users.noreply.github.com>
2022-02-23 12:59:30 +0800

    shift structs to correct places

--- pkg/browsers/osx.go
+++ pkg/browsers/osx.go
@@ -7,6 +7,35 @@ import (
 	"os/exec"
 )
 
+type plist struct {
+	//XMLName xml.Name `xml:"plist"`
+	Pdict Pdict `xml:"dict"`
+}
+
+type Pdict struct {
+	//XMLName xml.Name `xml:"dict"`
+	Key   string `xml:"key"`
+	Array Array  `xml:"array"`
+}
+
+type Array struct {
+	//XMLName xml.Name `xml:"array"`
+	Dict Dict `xml:"dict"`
+}
+
+type Dict struct {
+	//XMLName xml.Name `xml:"dict"`
+	Key     []string `xml:"key"`
+	Dict    IntDict  `xml:"dict"`
+	Strings []string `xml:"string"`
+}
+
+type IntDict struct {
+	//XMLName xml.Name `xml:"dict"`
+	Key     string `xml:"key"`
+	Strings string `xml:"string"`
+}
+
 func HandleOSXBrowserSearch() (string, error) {
 	//get home dir
 	home, err := os.UserHomeDir()
--- pkg/browsers/windows.go
+++ pkg/browsers/windows.go
@@ -1,34 +1,5 @@
 package browsers
 
-type plist struct {
-	//XMLName xml.Name `xml:"plist"`
-	Pdict Pdict `xml:"dict"`
-}
-
-type Pdict struct {
-	//XMLName xml.Name `xml:"dict"`
-	Key   string `xml:"key"`
-	Array Array  `xml:"array"`
-}
-
-type Array struct {
-	//XMLName xml.Name `xml:"array"`
-	Dict Dict `xml:"dict"`
-}
-
-type Dict struct {
-	//XMLName xml.Name `xml:"dict"`
-	Key     []string `xml:"key"`
-	Dict    IntDict  `xml:"dict"`
-	Strings []string `xml:"string"`
-}
-
-type IntDict struct {
-	//XMLName xml.Name `xml:"dict"`
-	Key     string `xml:"key"`
-	Strings string `xml:"string"`
-}
-
 func HandleWindowsBrowserSearch() (string, error) {
 	//TODO: automatic detection for windows
 	return "", nil
