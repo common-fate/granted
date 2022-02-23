69ffb0535dcfa2c76c4554efc484f325ccb03be5
meyerjrr <33018450+meyerjrr@users.noreply.github.com>
2022-02-23 10:13:55 +0800

    Jackmeyer/cf-589 include the region name in the firefox (#34)
    * added method to create unique name when opening diff regions
    
    * added check for filepath name
    
    * fixed path for chromium profile
    
    * added hash to chromium profile name

--- pkg/browsers/console.go
+++ pkg/browsers/console.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"hash/fnv"
 	"net/http"
 	"net/url"
 	"os"
@@ -124,8 +125,9 @@ func OpenWithChromiumProfile(url string, labels RoleLabels, selectedBrowser Brow
 		// A possible approach would be to open chrome in a headless way first then open it fully after setting the name
 
 		userDataPath := path.Join(grantedFolder, "chromium-profiles", fmt.Sprintf("%v", selectedBrowser))
+
 		cmd := exec.Command(chromePath,
-			fmt.Sprintf("--user-data-dir=%s", userDataPath), "--profile-directory="+labels.Profile, "--no-first-run", "--no-default-browser-check", url,
+			fmt.Sprintf("--user-data-dir=%s", userDataPath), "--profile-directory="+labels.MakeExternalProfileTitle(), "--no-first-run", "--no-default-browser-check", url,
 		)
 		err = cmd.Start()
 		if err != nil {
@@ -159,7 +161,7 @@ func OpenWithFirefoxContainer(urlString string, labels RoleLabels) error {
 		return errors.New("os not supported")
 	}
 
-	tabURL := fmt.Sprintf("ext+granted-containers:name=%s&url=%s", labels.Profile, url.QueryEscape(urlString))
+	tabURL := fmt.Sprintf("ext+granted-containers:name=%s&url=%s", labels.MakeExternalFirefoxTitle(), url.QueryEscape(urlString))
 	cmd := exec.Command(firefoxPath,
 		"--new-tab",
 		tabURL)
@@ -180,7 +182,33 @@ type Session struct {
 type RoleLabels struct {
 	// the name of the role
 	Profile string
+	Region  string
 }
+
+func (r *RoleLabels) MakeExternalFirefoxTitle() string {
+
+	if r.Region != "" {
+		return r.Profile + "(" + r.Region + ")"
+
+	}
+	return r.Profile
+}
+
+func (r *RoleLabels) MakeExternalProfileTitle() string {
+	n := r.Profile
+	if r.Region != "" {
+		n = r.Profile + "(" + r.Region + ")"
+
+	}
+
+	h := fnv.New32a()
+	h.Write([]byte(n))
+
+	hash := fmt.Sprint(h.Sum32())
+	return hash
+
+}
+
 type Browser int
 
 const (
