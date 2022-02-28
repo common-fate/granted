9a24a61124fe7c520f3b164c10d03aa0c0202177
Joshua Wilkes <14214200+JoshuaWilkes@users.noreply.github.com>
2022-02-28 15:10:26 +0800

    add saml2aws support via credential process

---
+++ pkg/cfaws/assumer_saml_2_aws.go
@@ -0,0 +1,66 @@
+package cfaws
+
+import (
+	"context"
+	"os/exec"
+	"strings"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/bigkevmcd/go-configparser"
+)
+
+// Implements Assumer
+type Saml2AwsAssumer struct {
+}
+
+// launch the saml2aws utility to generate the credentials
+// then fetch them from the environment for use
+func (s2a *Saml2AwsAssumer) AssumeTerminal(ctx context.Context, c *CFSharedConfig) (aws.Credentials, error) {
+	command := ""
+	for k, v := range c.RawConfig {
+		if k == "credential_process" && strings.HasPrefix(v, "saml2aws") {
+			command = v
+			break
+		}
+	}
+
+	// https://github.com/Versent/saml2aws#using-saml2aws-as-credential-process
+	// attempt to run the credential process for this profile
+	cmd := exec.Command(command)
+	err := cmd.Run()
+	if err != nil {
+		return aws.Credentials{}, err
+	}
+
+	// expoect that this process has written the credentials to a profile in the ~/.aws/credentials file
+	// we can now use the standard AWS sdk to get credentials for the profile
+	cfg, err := c.AwsConfig(ctx, false)
+	if err != nil {
+		return aws.Credentials{}, err
+	}
+	creds, err := aws.NewCredentialsCache(cfg.Credentials).Retrieve(ctx)
+	if err != nil {
+		return aws.Credentials{}, err
+	}
+	return creds, nil
+}
+
+func (s2a *Saml2AwsAssumer) AssumeConsole(ctx context.Context, c *CFSharedConfig) (aws.Credentials, error) {
+	return s2a.AssumeTerminal(ctx, c)
+}
+
+// A unique key which identifies this assumer e.g AWS-SSO or GOOGLE-AWS-AUTH
+func (s2a *Saml2AwsAssumer) Type() string {
+	return "SAML_2_AWS"
+}
+
+// inspect for any credential processes with the saml2aws tool
+func (s2a *Saml2AwsAssumer) ProfileMatchesType(rawProfile configparser.Dict, parsedProfile config.SharedConfig) bool {
+	for k, v := range rawProfile {
+		if k == "credential_process" && strings.HasPrefix(v, "saml2aws") {
+			return true
+		}
+	}
+	return false
+}
