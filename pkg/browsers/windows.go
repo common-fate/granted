b2d0577f509401b6e84c1aa54d98da68e5bc92fd
meyerjrr <jrmeyer339@gmail.com>
2022-02-28 13:32:04 +1100

    rename windows files

--- pkg/browsers/windows.go
+++ pkg/browsers/windows.go
@@ -1,7 +1,20 @@
-//go:build !windows
+//go:build windows
 
 package browsers
 
+import (
+	"golang.org/x/sys/windows/registry"
+)
+
 func HandleWindowsBrowserSearch() (string, error) {
-	return "", nil
+	// Lookup https handler in registry
+	k, err := registry.OpenKey(registry.CURRENT_USER, `SOFTWARE\\Microsoft\\Windows\\Shell\\Associations\\UrlAssociations\\https\\UserChoice`, registry.QUERY_VALUE)
+	if err != nil {
+		return "", err
+	}
+	kv, _, err := k.GetStringValue("ProgId")
+	if err != nil {
+		return "", err
+	}
+	return kv, nil
 }
