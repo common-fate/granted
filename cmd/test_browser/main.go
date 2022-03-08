68cfffc0e2362187a9faaea5ba0d3afdbbb8af8d
Joshua Wilkes <14214200+JoshuaWilkes@users.noreply.github.com>
2022-03-08 11:00:38 +0800

    test

--- cmd/test_browser/main.go
+++ cmd/test_browser/main.go
@@ -9,5 +9,7 @@ import (
 func main() {
 	shellEnv := os.Getenv("SHELL")
 	fmt.Printf("shellEnv: %v\n", shellEnv)
+	shellEnvo := os.Getenv("SHELLO")
+	fmt.Printf("shellEnvo: %v\n", shellEnvo)
 	os.Exit(0)
 }
--- scripts/test.sh
+++ scripts/test.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 echo "shell $SHELL"
+export SHELLO=hello
 ./bin/tbrowser
