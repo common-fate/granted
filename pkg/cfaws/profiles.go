0cdcf32557e4871d19520afe110c23e3b0e634b7
badideasforsale <80412585+badideasforsale@users.noreply.github.com>
2022-04-12 21:25:23 -0700

    Support profiles with (some) special characters (#131)
    * first attempt, test profiles with any char
    
    * tested what characters will break
    
    Co-authored-by: Brandon Sherman <brandon@temporal.io>

--- pkg/cfaws/profiles.go
+++ pkg/cfaws/profiles.go
@@ -58,9 +58,8 @@ func GetProfilesFromDefaultSharedConfig(ctx context.Context) (CFSharedConfigs, e
 		// Check if the section is prefixed with 'profile ' and that the profile has a name
 		if strings.HasPrefix(section, "profile ") && len(section) > 8 {
 			name := strings.TrimPrefix(section, "profile ")
-			illegalChars := ".,@#$%^&*()+=\\|]}[{;:'\"<>/?"
+			illegalChars := "\\][;'\"" // These characters break the config file format and should not be usable for profile names
 			if strings.ContainsAny(name, illegalChars) {
-				// The AWS SDK actually fails to parse profiles containing "." however the error it returns is not useful so we need to warn users of this
 				fmt.Fprintf(color.Error, "warning, profile: %s cannot be loaded because it contains one or more of: '%s' in the name, try replacing these with '-'\n", name, illegalChars)
 				continue
 			} else {
