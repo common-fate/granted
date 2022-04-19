d6ea05899acfceeddb60c076a8d86a7cad42170d
meyerjrr <jrmeyer339@gmail.com>
2022-04-19 15:28:39 +1000

    clean code

--- pkg/cfaws/assumer_aws_iam.go
+++ pkg/cfaws/assumer_aws_iam.go
@@ -94,7 +94,7 @@ func getFederationToken(ctx context.Context, c *CFSharedConfig) (aws.Credentials
 	name := "Granted@" + c.Name
 
 	if len(name) > 32 {
-		name = name[:32]
+		name = name[0:32]
 	}
 	out, err := client.GetFederationToken(ctx, &sts.GetFederationTokenInput{Name: aws.String(name)})
 	if err != nil {
