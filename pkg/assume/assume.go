a100ba5527e17f4daaa51cbd6a1bb05c8a910919
Chris Norman <17420369+chrnorm@users.noreply.github.com>
2022-06-16 19:35:04 +0100

    use GRANTED_ALIAS_CONFIGURED for check

--- pkg/assume/assume.go
+++ pkg/assume/assume.go
@@ -265,7 +265,11 @@ func EnvKeys(creds aws.Credentials, region string) []string {
 // Currently in windows, the grantedoutput is handled differently, as linux and mac support the exec cli flag whereas windows does not yet have support
 // this method may be changed in future if we implement support for "--exec" in windows
 func MakeGrantedOutput(s string) string {
-	if os.Getenv("NO_GRANTED_OUTPUT_PREFIX") == "true" {
+	// if the GRANTED_ALIAS_CONFIGURED env variable isn't set,
+	// we aren't running in the context of the `assume` shell script.
+	// If this is the case, don't add a prefix to the output as we don't have the
+	// wrapper shell script to parse it.
+	if os.Getenv("GRANTED_ALIAS_CONFIGURED") != "true" {
 		return ""
 	}
 	out := "GrantedOutput"
