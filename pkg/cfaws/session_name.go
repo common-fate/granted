896f3874b0b2b9be56415bdd103bf5a201f1614f
Joshua Wilkes <14214200+JoshuaWilkes@users.noreply.github.com>
2022-07-18 09:09:18 +0800

    comment

--- pkg/cfaws/session_name.go
+++ pkg/cfaws/session_name.go
@@ -4,6 +4,7 @@ import "github.com/segmentio/ksuid"
 
 // sessionName returns a unique session identifier for the aws console
 // this ensures that user activity can be easily audited per session
+// this uses the convenient ksuid library for generating unique IDs
 func sessionName() string {
 	return "gran-" + ksuid.New().String()
 }
