8dccede8c1d1ceb2143c83fd3aab9d661a6a6852
Alex Jurkiewicz <alex@jurkiewi.cz>
2022-07-18 10:02:19 +1000

    assume: Support short region names (eg 'use1') (#179)
    * assume: Support short region names (eg 'use1')
    
    * Apply suggestions from code review
    
    Co-authored-by: JoshuaWilkes <14214200+JoshuaWilkes@users.noreply.github.com>
    
    Co-authored-by: JoshuaWilkes <14214200+JoshuaWilkes@users.noreply.github.com>

--- pkg/browsers/console.go
+++ pkg/browsers/console.go
@@ -10,6 +10,7 @@ import (
 	"os"
 	"os/exec"
 	"path"
+	"strconv"
 	"strings"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
@@ -25,7 +26,7 @@ var ServiceMap = map[string]string{
 	"":               "console",
 	"ec2":            "ec2/v2",
 	"sso":            "singlesignon",
- 	"ecs":            "ecs",
+	"ecs":            "ecs",
 	"eks":            "eks",
 	"athena":         "athena",
 	"cloudmap":       "cloudmap",
@@ -56,7 +57,7 @@ var ServiceMap = map[string]string{
 	"param":          "systems-manager/parameters",
 	"redshift":       "redshiftv2",
 	"sagemaker":      "sagemaker",
-	"ssm":            "systems-manager",	
+	"ssm":            "systems-manager",
 }
 
 var globalServiceMap = map[string]bool{
@@ -270,6 +271,11 @@ func MakeUrl(sess Session, opts BrowserOpts, service string, region string) (str
 		return "", err
 	}
 
+	region, err = expandRegion(region)
+	if err != nil {
+		return "", fmt.Errorf("couldn't parse region %s: %v", region, err)
+	}
+
 	partition := GetPartitionFromRegion(region)
 	debug.Fprintf(debug.VerbosityDebug, color.Error, "Partition is detected as %s for region %s...\n", partition, region)
 
@@ -365,11 +371,133 @@ func LaunchConsoleSession(sess Session, opts BrowserOpts, service string, region
 	}
 }
 
-func makeDestinationURL(service string, region string) (string, error) {
-
+func expandRegion(region string) (string, error) {
+	// Region could come in one of three formats:
+	// 1. No region specified
 	if region == "" {
-		region = "us-east-1"
+		return "us-east-1", nil
 	}
+	// 2. A fully-qualified region. Assume that if there's one dash, it's valid.
+	if strings.Contains(region, "-") {
+		return region, nil
+	}
+	var major, minor, num string
+	idx := 1 // Number of characters consumed from region
+	// 3. Otherwise, we have a shortened region, like ue1
+	if len(region) < 2 {
+		return "", fmt.Errorf("region too short, needs at least two characters (eg ue)")
+	}
+	// Region might be one or two letters
+	switch region[0] {
+	case 'u':
+		{
+			major = "us"
+			if region[1] == 'g' {
+				major = "us-gov"
+				idx += 1
+			} else if region[1] == 's' {
+				// This will break if us-southeast-1 is ever created
+				idx += 1
+			}
+		}
+	case 'e':
+		{
+			major = "eu"
+		}
+	case 'a':
+		{
+			major = "ap"
+			if region[1] == 'f' {
+				major = "af"
+				idx += 1
+			} else if region[1] == 'p' {
+				idx += 1
+			}
+		}
+	case 'c':
+		{
+			major = "ca"
+			if region[1] == 'n' {
+				major = "cn"
+				idx += 1
+			} else if region[1] == 'a' {
+				idx += 1
+			}
+		}
+	case 'm':
+		{
+			major = "me"
+			// This will break if me-east-1 is ever created
+			if region[1] == 'e' {
+				idx += 1
+			}
+		}
+	case 's':
+		{
+			major = "sa"
+			if region[1] == 'a' {
+				idx += 1
+			}
+		}
+	default:
+		{
+			return "", fmt.Errorf("unknown region major (hint: try using the first letter of the region)")
+		}
+	}
+	region = region[idx:]
+	idx = 1
+	// Location might be one or two letters (n, nw)
+	switch region[0] {
+	case 'n', 's':
+		{
+			if region[0] == 'n' {
+				minor = "north"
+			} else {
+				minor = "south"
+			}
+			if len(region) > 1 {
+				if region[1] == 'w' {
+					minor += "west"
+					idx += 1
+
+				} else if region[1] == 'e' {
+					minor += "east"
+					idx += 1
+				}
+			}
+		}
+	case 'e':
+		{
+			minor = "east"
+		}
+	case 'w':
+		{
+			minor = "west"
+		}
+	case 'c':
+		{
+			minor = "central"
+		}
+	default:
+		{
+			return "", fmt.Errorf("unknown region minor in %s (found major: %s)", region, major)
+		}
+	}
+	region = region[idx:]
+	if len(region) > 0 {
+		_, err := strconv.Atoi(region)
+		if err != nil {
+			return "", fmt.Errorf("unknown region number in %s (found major: %s, minor: %s)", region, major, minor)
+		}
+		num = region
+	} else {
+		num = "1"
+	}
+
+	return fmt.Sprintf("%s-%s-%s", major, minor, num), nil
+}
+
+func makeDestinationURL(service string, region string) (string, error) {
 	partition := GetPartitionFromRegion(region)
 	prefix := partition.ConsoleHostString()
 
