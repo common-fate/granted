68ac3cb518272a493ac04652654d865bc4921443
jordiup <jordi.hermoso.up@gmail.com>
2022-09-09 11:22:16 +0800

    Update credentialsprocess.go

--- pkg/granted/credentialsprocess.go
+++ pkg/granted/credentialsprocess.go
@@ -1,11 +1,10 @@
 package granted
 
 import (
-	"errors"
+	"encoding/json"
 	"fmt"
 	"log"
 	"os"
-	"os/exec"
 	"time"
 
 	"github.com/aws/smithy-go"
@@ -41,73 +40,79 @@ var CredentialsProcess = cli.Command{
 		}
 
 		// Check if the session can be assumed
-		err := CheckIfRequiresApproval(c, profileName)
+		// err := CheckIfRequiresApproval(c, profileName)
+
+		var profile *cfaws.Profile
+
+		profiles, err := cfaws.LoadProfiles()
 		if err != nil {
-			os.Exit(1)
-			fmt.Fprintln(color.Error, "Unhandled exception while initializing SSO")
+			return err
 		}
 
-		// Note: an option here that hooks into the native aws sso workflow
-		fmt.Print(`{
-			"Version": 1,
-			"AccessKeyId": "an AWS access key",
-			"SecretAccessKey": "your AWS secret access key",
-			"SessionToken": "the AWS session token for temporary credentials", 
-			"Expiration": "ISO8601 timestamp when the credentials expire"
-		  }`)
-		return nil
+		if profiles.HasProfile(profileName) {
+			profile, err = profiles.LoadInitialisedProfile(c.Context, profileName)
+			if err != nil {
+				return err
+			}
 
-		// Yes it (now) can be assumed, run standard `aws aws-sso-credential-process..`
-		// out, err := exec.Command("aws-sso-credential-process", "credential-process", "--profile", profileName).Output()
-		out, err := exec.Command("aws-sso-credential-process", "credential-process", "--profile", profileName).Output()
-		if err != nil {
-			log.Fatalln("Error running native aws-sso-credential-process with profile: "+profileName, err.Error())
-			log.Fatal(err)
+			creds, err := profile.AssumeTerminal(c.Context, cfaws.ConfigOpts{Duration: time.Hour})
+
+			if err != nil {
+				serr, ok := err.(*smithy.OperationError)
+				if ok {
+					if serr.ServiceID == "SSO" {
+						baseUrl, ruleId := "internal.prod.granted.run/", "rul_2BtW97o6jTacUuzxNJZorACn5v0"
+						// Guide user to common fate if error
+						s := fmt.Sprintf(color.YellowString("\n\nYou need to request access to this role:")+"\nhttps://%s/access/request/%s\n", baseUrl, ruleId)
+
+						log.Fatal(s)
+					}
+				}
+				return err
+			}
+
+			if err != nil {
+				os.Exit(1)
+				fmt.Fprintln(color.Error, "Unhandled exception while initializing SSO")
+			}
+
+			var out AWSCredsStdOut
+
+			out.AccessKeyID = creds.AccessKeyID
+			out.Expiration = creds.Expires.Format(time.RFC3339)
+			out.SecretAccessKey = creds.SecretAccessKey
+			out.SessionToken = creds.SessionToken
+			out.Version = 1
+
+			jsonOut, err := json.Marshal(out)
+			if err != nil {
+				fmt.Printf("Error: %s", err)
+			}
+
+			// Fetch the creds
+			// assume.AssumeCommand2(c)
+
+			// Yes it (now) can be assumed, run standard `aws aws-sso-credential-process..`
+			// out, err := exec.Command("aws-sso-credential-process", "credential-process", "--profile", profileName).Output()
+			// out, err := exec.Command("aws-sso-util", "credential-process", "--profile", profileName).Output()
+			// if err != nil {
+			// 	log.Fatalln("\nError running native aws-sso-credential-process with profile: "+profileName, err.Error())
+			// 	log.Fatal(err)
+			// }
+			// Export the credentials in json format
+			fmt.Print(string(jsonOut))
 		}
-		// Export the credentials in json format
-		fmt.Print(string(out))
 
 		return nil
 	},
 }
 
-/**
-* IO = return nil, log fatal error if User does not have access to the SSO role
-* IO = return error, log nothing if unhandled exception
- */
-func CheckIfRequiresApproval(c *cli.Context, profileName string) error {
+// /**
+// * IO = return nil, log fatal error if User does not have access to the SSO role
+// * IO = return error, log nothing if unhandled exception
+//  */
+// func CheckIfRequiresApproval(c *cli.Context, profileName string) error {
 
-	var profile *cfaws.Profile
+// }
 
-	profiles, err := cfaws.LoadProfiles()
-	if err != nil {
-		return err
-	}
-
-	if profiles.HasProfile(profileName) {
-		profile, err = profiles.LoadInitialisedProfile(c.Context, profileName)
-		if err != nil {
-			return err
-		}
-
-		_, err := profile.AssumeTerminal(c.Context, cfaws.ConfigOpts{Duration: time.Hour})
-
-		if err != nil {
-			serr, ok := err.(*smithy.OperationError)
-			if ok {
-				if serr.ServiceID == "SSO" {
-					baseUrl, ruleId := "internal.prod.granted.run/", "rul_2BtW97o6jTacUuzxNJZorACn5v0"
-					// Guide user to common fate if error
-					s := fmt.Sprintf(color.YellowString("\n\nYou need to request access to this role:")+"\nhttps://%s/access/request/%s\n", baseUrl, ruleId)
-
-					log.Fatal(s)
-				}
-			}
-			return err
-		}
-		return nil
-	}
-	return errors.New("unhandled error, could not assume profile")
-}
-
-// @TODO: we may also want to add an automated process that handles sync to ensure the config is never stale
+// // @TODO: we may also want to add an automated process that handles sync to ensure the config is never stale
