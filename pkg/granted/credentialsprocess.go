848f5b190867fb7836ce76c9021e401a34ea1ce8
Manish Chaulagain <eddie@MacBook-Pro-2.local>
2022-09-14 16:12:56 +1000

    Make credential_process work with plain text SSO

--- pkg/granted/credentialsprocess.go
+++ pkg/granted/credentialsprocess.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"strings"
 	"time"
 
 	"github.com/aws/smithy-go"
@@ -30,12 +31,14 @@ var CredentialsProcess = cli.Command{
 	Usage:       "",
 	Hidden:      true,
 	Subcommands: []*cli.Command{&ConfigSetup},
-	Flags:       []cli.Flag{&cli.StringFlag{Name: "profile"}, &cli.StringFlag{Name: "rule"}},
+	Flags:       []cli.Flag{&cli.StringFlag{Name: "profile", Required: true}, &cli.StringFlag{Name: "rule"}},
 	Action: func(c *cli.Context) error {
 
 		url := ""
 		profileName := c.String("profile")
-		// ruleId := c.String("rule")
+		if !strings.HasPrefix(profileName, "granted.") {
+			log.Fatal("Profile name must start with 'granted.' prefix. Check your config file to verify.")
+		}
 
 		conf, err := config.Load()
 		if err != nil {
@@ -46,62 +49,48 @@ var CredentialsProcess = cli.Command{
 			log.Fatal("It looks like you haven't setup your GrantedApprovalsUrl\nTo do so please run: " + c.App.Name + " setup")
 		}
 
-		if profileName == "" {
-			log.Fatalln("Mis-configured aws config file.\n--profile flag must be passed")
-		}
-
-		var profile *cfaws.Profile
-
 		profiles, err := cfaws.LoadProfiles()
 		if err != nil {
-			return err
+			log.Fatal(err)
 		}
 
-		if profiles.HasProfile(profileName) {
-			profile, err = profiles.LoadInitialisedProfile(c.Context, profileName)
-			if err != nil {
-				return err
-			}
+		profileNameWithoutPrefix := strings.SplitAfter(profileName, ".")[1]
+		profile, err := profiles.LoadInitialisedProfile(c.Context, profileNameWithoutPrefix)
 
-			creds, err := profile.AssumeTerminal(c.Context, cfaws.ConfigOpts{Duration: time.Hour})
+		creds, err := profile.AssumeTerminal(c.Context, cfaws.ConfigOpts{Duration: time.Hour})
+		if err != nil {
+			serr, ok := err.(*smithy.OperationError)
+			if ok {
+				// @TODO: this still seems to trigger when ...
+				// 1. revoke any access to cf-dev
+				// 2. request access and wait for grant = active
+				// 3. try run `aws s3 ls --profile granted.cf-dev`
+				// 4. it should auto assume the role, but instead it throws below error \/ \/
+				if serr.ServiceID == "SSO" {
+					baseUrl, ruleId := url, "rul_2BtW97o6jTacUuzxNJZorACn5v0"
+					// Guide user to common fate if error
+					s := fmt.Sprintf(color.YellowString("\n\nYou need to request access to this role:")+"\n%s/access/request/%s\n", baseUrl, ruleId)
 
-			if err != nil {
-				serr, ok := err.(*smithy.OperationError)
-				if ok {
-					// @TODO: this still seems to trigger when ...
-					// 1. revoke any access to cf-dev
-					// 2. request access and wait for grant = active
-					// 3. try run `aws s3 ls --profile granted.cf-dev`
-					// 4. it should auto assume the role, but instead it throws below error \/ \/
-					if serr.ServiceID == "SSO" {
-						baseUrl, ruleId := url, "rul_2BtW97o6jTacUuzxNJZorACn5v0"
-						// Guide user to common fate if error
-						s := fmt.Sprintf(color.YellowString("\n\nYou need to request access to this role:")+"\n%s/access/request/%s\n", baseUrl, ruleId)
-
-						log.Fatal(s)
-					}
-				} else {
-					log.Fatalln("\nError running credential with profile: "+profileName, err.Error())
+					log.Fatal(s)
 				}
+			} else {
+				log.Fatalln("\nError running credential with profile: "+profileName, err.Error())
 			}
-
-			var out AWSCredsStdOut
-
-			out.AccessKeyID = creds.AccessKeyID
-			out.Expiration = creds.Expires.Format(time.RFC3339)
-			out.SecretAccessKey = creds.SecretAccessKey
-			out.SessionToken = creds.SessionToken
-			out.Version = 1
-
-			jsonOut, err := json.Marshal(out)
-			if err != nil {
-				log.Fatalln("\nUnhandled error when unmarshalling json creds")
-			}
-			fmt.Print(string(jsonOut))
 		}
+
+		var out AWSCredsStdOut
+		out.AccessKeyID = creds.AccessKeyID
+		out.Expiration = creds.Expires.Format(time.RFC3339)
+		out.SecretAccessKey = creds.SecretAccessKey
+		out.SessionToken = creds.SessionToken
+		out.Version = 1
+
+		jsonOut, err := json.Marshal(out)
+		if err != nil {
+			log.Fatalln("\nUnhandled error when unmarshalling json creds")
+		}
+		fmt.Println(string(jsonOut))
 
 		return nil
 	},
 }
-
-// @TODO: we may also want to add an automated process that handles sync to ensure the config is never stale
