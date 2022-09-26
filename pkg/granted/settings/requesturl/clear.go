6022f0678a60a3e64f4ec5d517f080d63dfb440a
Manish Chaulagain <eddie@MacBook-Pro-3.local>
2022-09-26 14:45:47 +1000

    CLI-31 Make set & clear subcmd instead of flag for requestURl cmd

---
+++ pkg/granted/settings/requesturl/clear.go
@@ -0,0 +1,29 @@
+package requesturl
+
+import (
+	"fmt"
+
+	grantedConfig "github.com/common-fate/granted/pkg/config"
+	"github.com/pkg/errors"
+	"github.com/urfave/cli/v2"
+)
+
+var clearRequestURLCommand = cli.Command{
+	Name:  "clear",
+	Usage: "Clears the current request url",
+	Action: func(c *cli.Context) error {
+		gConf, err := grantedConfig.Load()
+		if err != nil {
+			return errors.Wrap(err, "unable to load granted config")
+		}
+
+		gConf.AccessRequestURL = ""
+		if err := gConf.Save(); err != nil {
+			return errors.Wrap(err, "saving config")
+		}
+
+		fmt.Println("Successfully cleared the request url")
+		return nil
+
+	},
+}
--- pkg/granted/settings/settings.go
+++ pkg/granted/settings/settings.go
@@ -2,11 +2,13 @@ package settings
 
 import (
 	"github.com/urfave/cli/v2"
+
+	"github.com/common-fate/granted/pkg/granted/settings/requesturl"
 )
 
 var SettingsCommand = cli.Command{
 	Name:        "settings",
 	Usage:       "Manage Granted settings",
-	Subcommands: []*cli.Command{&PrintCommand, &ProfileOrderingCommand, &ExportSettingsCommand, &RequestURLCommand},
+	Subcommands: []*cli.Command{&PrintCommand, &ProfileOrderingCommand, &ExportSettingsCommand, &requesturl.Commands},
 	Action:      PrintCommand.Action,
 }
