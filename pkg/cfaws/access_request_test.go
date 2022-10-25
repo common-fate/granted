b1a973fc2e415eb8b920e267f459842e9bdddae9
Josh Wilkes <14214200+JoshuaWilkes@users.noreply.github.com>
2022-10-25 15:03:06 +0800

    update access request test

--- pkg/cfaws/access_request_test.go
+++ pkg/cfaws/access_request_test.go
@@ -4,7 +4,10 @@ import (
 	"testing"
 
 	"github.com/bigkevmcd/go-configparser"
+	"github.com/common-fate/clio"
 	grantedConfig "github.com/common-fate/granted/pkg/config"
+	"github.com/pkg/errors"
+	"github.com/stretchr/testify/assert"
 )
 
 func Test_parseURLFlagFromConfig(t *testing.T) {
@@ -54,11 +57,7 @@ func Test_parseURLFlagFromConfig(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got, err := parseURLFlagFromConfig(tt.args.rawConfig)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("parseURLFlagFromConfig() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
+			got := parseURLFlagFromConfig(tt.args.rawConfig)
 			if got != tt.want {
 				t.Errorf("parseURLFlagFromConfig() = %v, want %v", got, tt.want)
 			}
@@ -76,7 +75,7 @@ func TestGetGrantedApprovalsURL(t *testing.T) {
 	tests := []struct {
 		name    string
 		args    args
-		want    string
+		want    *clio.CLIError
 		wantErr bool
 	}{
 		{
@@ -88,8 +87,13 @@ func TestGetGrantedApprovalsURL(t *testing.T) {
 				SSORoleName:  "test",
 				SSOAccountId: "123456789012",
 			},
-			want: `You need to request access to this role:
-https://example.com/access?accountId=123456789012&permissionSetArn.label=test&type=commonfate%2Faws-sso`,
+			want: &clio.CLIError{
+				Err: "test error",
+				Messages: []clio.Printer{
+					clio.WarnMsg("You need to request access to this role:"),
+					clio.WarnlnMsg("https://example.com/access?accountId=123456789012&permissionSetArn.label=test&type=commonfate%2Faws-sso"),
+				},
+			},
 		},
 		{
 			name: "url flag precedence",
@@ -104,28 +108,33 @@ https://example.com/access?accountId=123456789012&permissionSetArn.label=test&ty
 				SSORoleName:  "test",
 				SSOAccountId: "123456789012",
 			},
-			want: `You need to request access to this role:
-https://override.example.com/access?accountId=123456789012&permissionSetArn.label=test&type=commonfate%2Faws-sso`,
+			want: &clio.CLIError{
+				Err: "test error",
+				Messages: []clio.Printer{
+					clio.WarnMsg("You need to request access to this role:"),
+					clio.WarnlnMsg("https://override.example.com/access?accountId=123456789012&permissionSetArn.label=test&type=commonfate%2Faws-sso"),
+				},
+			},
 		},
 		{
 			name: "display prompt if no URL is set",
 			args: args{
 				gConf: grantedConfig.Config{},
 			},
-			want: `Granted Approvals URL not configured. 
-Set up a URL to request access to this role with 'granted settings request-url set <YOUR_GRANTED_APPROVALS_URL'`,
+			want: &clio.CLIError{
+				Err: "test error",
+				Messages: []clio.Printer{
+					clio.InfoMsg("It looks like you don't have the right permissions to access this role"),
+					clio.InfoMsg("If you are using Granted Approvals to manage this role you can configure the Granted CLI with a request URL so that you can be directed to your Granted Approvals instance to make a new access request the next time you have this error"),
+					clio.InfoMsg("To configure a URL to request access to this role with 'granted settings request-url set <YOUR_GRANTED_APPROVALS_URL'"),
+				},
+			},
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got, err := GetGrantedApprovalsURL(tt.args.rawConfig, tt.args.gConf, tt.args.SSORoleName, tt.args.SSOAccountId)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("GetGrantedApprovalsURL() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-			if got != tt.want {
-				t.Errorf("GetGrantedApprovalsURL() = %v, want %v", got, tt.want)
-			}
+			err := FormatAWSErrorWithGrantedApprovalsURL(errors.New("test error"), tt.args.rawConfig, tt.args.gConf, tt.args.SSORoleName, tt.args.SSOAccountId)
+			assert.Equal(t, tt.want, err)
 		})
 	}
 }
