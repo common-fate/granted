b0a1ae6655f66d97b734606d00ac2ee7db412025
Manish Chaulagain <eddie@MacBook-Pro-5.local>
2022-11-07 16:29:11 +1100

    CLI-68 Bug fix of autogenerated text added when add cmd is reran with profile registry already present

--- pkg/granted/registry/add.go
+++ pkg/granted/registry/add.go
@@ -55,6 +55,7 @@ var AddCommand = cli.Command{
 				continue
 			}
 
+			clio.Debug("parsing the provided url to get host, organization and repo name")
 			url, err := parseGitURL(repoURL)
 			if err != nil {
 				return err
@@ -125,8 +126,12 @@ var AddCommand = cli.Command{
 			}
 
 			isFirstSection := false
-			if index == 0 {
-				isFirstSection = true
+			// if there are no granted registry setup yet then
+			// check if this is the first index
+			if len(gConf.ProfileRegistryURLS) <= 0 {
+				if index == 0 {
+					isFirstSection = true
+				}
 			}
 
 			awsConfigPath, err := getDefaultAWSConfigLocation()
