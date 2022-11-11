6d98964bfb2f0c5cb0d577d3efdf942b4e15d287
Manish Chaulagain <eddie@MacBook-Pro-5.local>
2022-11-11 15:19:23 +1100

    Fix parseGitURL logic

--- pkg/granted/registry/git.go
+++ pkg/granted/registry/git.go
@@ -39,8 +39,13 @@ func (g *GitURL) GetURL() string {
 	return split[0] + ".git"
 }
 
+// The passed string is matched with regex that checks whether the string is valid git url
+// also checks if the provided string consist of subfolder or yaml spcific path
+// the regex checks if optional group (<SUBPATH> or <WITH_YML>) is present.
+// if string consist of .y(a)ml then the group will be matched with <WITH_YML> group
+// if string doesn't consist of .y(a)ml but does consist of subfolder then <SUBPATH> group will be matched.
 func parseGitURL(repoURL string) (GitURL, error) {
-	re := regexp.MustCompile(`((git@|http(s)?:\/\/)(?P<HOST>[\w\.@]+)(\/|:))(?P<ORG>[\w,\-,\_]+)\/(?P<REPO>[\w,\-,\_]+)(.git){0,1}(\/){0,1}((?P<SUBPATH>.+)?(\/)?(?P<FILE>.+yml)?)?`)
+	re := regexp.MustCompile(`((git@|http(s)?:\/\/)(?P<HOST>[\w\.@]+)(\/|:))(?P<ORG>[\w,\-,\_]+)\/(?P<REPO>[\w,\-,\_]+)(.git){0,1}(\/)?((?P<WITH_YML>.+ya?ml)|(?P<SUBPATH>(\w+(\/)?)*))`)
 
 	if re.MatchString(repoURL) {
 		matches := re.FindStringSubmatch(repoURL)
@@ -48,18 +53,28 @@ func parseGitURL(repoURL string) (GitURL, error) {
 		orgIndex := re.SubexpIndex("ORG")
 		repoIndex := re.SubexpIndex("REPO")
 		subpathIndex := re.SubexpIndex("SUBPATH")
+		filepathIndex := re.SubexpIndex("WITH_YML")
 
-		subpath := matches[subpathIndex]
-		var configFileName = ""
-		var folder = ""
+		withConfigFilePath := matches[filepathIndex]
 
-		if subpath != "" {
-			if strings.HasSuffix(subpath, ".yml") {
-				configFileName = subpath[strings.LastIndex(subpath, "/")+1:]
-				folder = subpath[:strings.LastIndex(subpath, "/")]
-			} else {
-				folder = subpath
+		var filename string = ""
+		var subpath string = ""
+
+		// filename specific group might not contain subpath
+		// example: /team_a/team_b/granted.yml
+		// example: .git/granted.yml
+		if withConfigFilePath != "" {
+			lastIndex := strings.LastIndex(withConfigFilePath, "/")
+
+			// there is a subpath
+			if lastIndex > 0 {
+				subpath = withConfigFilePath[:lastIndex+1]
 			}
+
+			// filename is all the string after the last slash
+			filename = withConfigFilePath[lastIndex+1:]
+		} else {
+			subpath = matches[subpathIndex]
 		}
 
 		return GitURL{
@@ -67,8 +82,8 @@ func parseGitURL(repoURL string) (GitURL, error) {
 			Host:        matches[hostIndex],
 			Org:         matches[orgIndex],
 			Repo:        matches[repoIndex],
-			Subpath:     folder,
-			Filename:    configFileName,
+			Subpath:     subpath,
+			Filename:    filename,
 		}, nil
 
 	}
