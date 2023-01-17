00d3e2ada8f13813b2d13c401a9714b24ecdc8a4
JoshuaWilkes <14214200+JoshuaWilkes@users.noreply.github.com>
2023-01-17 15:03:30 +0800

    Add granted console command (#334)
    * Implement console command
    
    * add firefox support
    
    * can also launch the browser

--- pkg/granted/entrypoint.go
+++ pkg/granted/entrypoint.go
@@ -28,7 +28,7 @@ func GetCliApp() *cli.App {
 		UsageText:            "granted [global options] command [command options] [arguments...]",
 		Version:              build.Version,
 		HideVersion:          false,
-		Commands:             []*cli.Command{&DefaultBrowserCommand, &settings.SettingsCommand, &CompletionCommand, &TokenCommand, &SSOTokensCommand, &UninstallCommand, &SSOCommand, &CredentialsCommand, middleware.WithBeforeFuncs(&CredentialProcess, middleware.WithAutosync()), &registry.ProfileRegistryCommand},
+		Commands:             []*cli.Command{&DefaultBrowserCommand, &settings.SettingsCommand, &CompletionCommand, &TokenCommand, &SSOTokensCommand, &UninstallCommand, &SSOCommand, &CredentialsCommand, middleware.WithBeforeFuncs(&CredentialProcess, middleware.WithAutosync()), &registry.ProfileRegistryCommand, &ConsoleCommand},
 		EnableBashCompletion: true,
 		Before: func(c *cli.Context) error {
 			clio.SetLevelFromEnv("GRANTED_LOG")
