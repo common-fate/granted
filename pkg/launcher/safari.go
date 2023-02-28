c1760ddf945e7d92222e388c56a28c7bab1e1e9a
Artur Tamborski <atamborski@cleeng.com>
2023-02-28 10:35:48 +0100

    modify a comment

--- pkg/launcher/safari.go
+++ pkg/launcher/safari.go
@@ -3,7 +3,7 @@ package launcher
 import "github.com/common-fate/granted/pkg/browser"
 
 // Open calls the 'open' command to open a URL.
-// This is the same command as when you run 'open https://commonfate.io'
+// This is the same command as when you run 'open -a Safari https://commonfate.io'
 // in your own terminal.
 type Safari struct{}
 
