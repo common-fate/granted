77b0c7e4cc815723e49bbb6f519092995c792a3d
Chris Norman <17420369+chrnorm@users.noreply.github.com>
2023-04-11 17:49:50 +0100

    fix broken smoke tests (#399)
    * attempt to fix broken smoke tests
    
    * attempt to fix tests
    
    * Revert "attempt to fix tests"
    
    This reverts commit ff52ca264badf5b78044b27dee5d71226c309cf2.
    
    * Revert "attempt to fix broken smoke tests"
    
    This reverts commit 9a7c74d047cc760483000f6e88c5f4af86aab673.
    
    * Revert "Add fallback support for any POSIX-compatible shell (#389)"
    
    This reverts commit 91242aa665a3614ffc9698ac8886b2ebaebd1f9b.

--- pkg/alias/alias.go
+++ pkg/alias/alias.go
@@ -30,9 +30,9 @@ func init() {
 }
 
 const fishAlias = `alias assume="source /usr/local/bin/assume.fish"`
-const defaultAlias = `alias assume=". assume"`
+const defaultAlias = `alias assume="source assume"`
 const devFishAlias = `alias dassume="source /usr/local/bin/dassume.fish"`
-const devDefaultAlias = `alias dassume=". dassume"`
+const devDefaultAlias = `alias dassume="source dassume"`
 
 func GetDefaultAlias() string {
 	if build.IsDev() {
@@ -77,9 +77,7 @@ func GetShellFromShellEnv(shellEnv string) (string, error) {
 
 	} else if strings.Contains(shellEnv, "zsh") {
 		return "zsh", nil
-	} else if strings.Contains(shellEnv, "sh") {
-		// Fallback to POSIX
-		return "posix", nil
+
 	} else {
 		return "", fmt.Errorf("we couldn't detect your shell type (%s). Please follow the steps at https://docs.commonfate.io/granted/internals/shell-alias to assume roles with Granted", shellEnv)
 	}
@@ -98,8 +96,6 @@ func GetShellAlias(shell string) (Config, error) {
 		file, err = shells.GetBashConfigFile()
 	case "zsh":
 		file, err = shells.GetZshConfigFile()
-	case "posix":
-		file, err = shells.GetPosixConfigFile()
 	default:
 		err = &ErrShellNotSupported{Shell: shell}
 	}
