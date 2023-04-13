28bb2d4b1e960086eef1ef1e3ba9da4bdc6bb869
Luke Young <lyoung@confluent.io>
2023-04-12 21:58:07 -0700

    Replace assumego with a symlink to fix keychain ACLs

--- cmd/granted/main.go
+++ cmd/granted/main.go
@@ -2,19 +2,29 @@ package main
 
 import (
 	"os"
+	"path/filepath"
 
 	"github.com/common-fate/clio"
 	"github.com/common-fate/clio/clierr"
 	"github.com/common-fate/granted/internal/build"
+	"github.com/common-fate/granted/pkg/assume"
 	"github.com/common-fate/granted/pkg/granted"
 	"github.com/common-fate/updatecheck"
+	"github.com/urfave/cli/v2"
 )
 
 func main() {
 	updatecheck.Check(updatecheck.GrantedCLI, build.Version, !build.IsDev())
 	defer updatecheck.Print()
 
-	app := granted.GetCliApp()
+	// Use a single binary to keep keychain ACLs simple, swapping behavior via argv[0]
+	var app *cli.App
+	switch filepath.Base(os.Args[0]) {
+	case "assumego", "assumego.exe", "dassumego", "dassumego.exe":
+		app = assume.GetCliApp()
+	default:
+		app = granted.GetCliApp()
+	}
 
 	err := app.Run(os.Args)
 	if err != nil {
