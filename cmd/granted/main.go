45dd7bec9427a7d487283372c9c7eede83f3d6f6
Chris Norman <17420369+chrnorm@users.noreply.github.com>
2023-05-31 19:19:22 +0100

    fix text disappearing in some instances due to spinner (#424)

--- cmd/granted/main.go
+++ cmd/granted/main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"os"
+	"os/signal"
 	"path/filepath"
+	"runtime"
+	"syscall"
 
 	"github.com/common-fate/clio"
 	"github.com/common-fate/clio/clierr"
@@ -16,6 +20,19 @@ import (
 func main() {
 	updatecheck.Check(updatecheck.GrantedCLI, build.Version, !build.IsDev())
 	defer updatecheck.Print()
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, syscall.SIGINT, syscall.SIGTERM)
+	go func() {
+		<-c
+		// restore cursor in case spinner gets stuck
+		// https://github.com/apppackio/apppack/commit/a711e55238af2402b4b027a73fccc663ec7ba0f4
+		// https://github.com/briandowns/spinner/issues/122
+		if runtime.GOOS != "windows" {
+			fmt.Fprint(os.Stdin, "\033[?25h")
+		}
+		os.Exit(130)
+	}()
 
 	// Use a single binary to keep keychain ACLs simple, swapping behavior via argv[0]
 	var app *cli.App
