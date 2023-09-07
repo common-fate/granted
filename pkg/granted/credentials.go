d2f28290ec9488a6033068ecbd1d0a34b0d14edb
Manish Chaulagain <manish23chaulagain@gmail.com>
2023-09-07 16:16:44 +1000

    Add granted credentials import-from-env subcommand to load new profile with credentials from environment

--- pkg/granted/credentials.go
+++ pkg/granted/credentials.go
@@ -3,12 +3,14 @@ package granted
 import (
 	"context"
 	"fmt"
+	"os"
 	"strings"
 
 	"github.com/AlecAivazis/survey/v2"
 	"github.com/AlecAivazis/survey/v2/core"
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/credentials"
 	"github.com/aws/aws-sdk-go-v2/service/iam"
 	"gopkg.in/ini.v1"
 
@@ -23,7 +25,7 @@ import (
 var CredentialsCommand = cli.Command{
 	Name:        "credentials",
 	Usage:       "Manage secure IAM credentials",
-	Subcommands: []*cli.Command{&AddCredentialsCommand, &ImportCredentialsCommand, &UpdateCredentialsCommand, &ListCredentialsCommand, &RemoveCredentialsCommand, &ExportCredentialsCommand, &RotateCredentialsCommand},
+	Subcommands: []*cli.Command{&AddCredentialsCommand, &ImportCredentialsCommand, &UpdateCredentialsCommand, &ListCredentialsCommand, &RemoveCredentialsCommand, &ExportCredentialsCommand, &RotateCredentialsCommand, &ImportCredFromEnvCommand},
 }
 
 var AddCredentialsCommand = cli.Command{
@@ -578,5 +580,88 @@ var RotateCredentialsCommand = cli.Command{
 		clio.Successf("Access Key of '%s' profile has been successfully rotated and updated in secure storage\n", profileName)
 
 		return nil
+	},
+}
+
+var ImportCredFromEnvCommand = cli.Command{
+	Name:  "import-from-env",
+	Usage: "Create a new AWS config profile with IAM credentials imported from environment. You must have $AWS_ACCESS_KEY_ID and $AWS_SECRET_ACCESS_KEY set in  your environment",
+	Flags: []cli.Flag{
+		&cli.StringFlag{Name: "profile", Required: true},
+	},
+	Action: func(c *cli.Context) error {
+		ctx := c.Context
+
+		accessKeyFromEnv, accessKeyFromEnvExists := os.LookupEnv("AWS_ACCESS_KEY_ID")
+		secretAccessKeyFromEnv, secretAccessKeyFromEnvExists := os.LookupEnv("AWS_ACCESS_KEY_ID")
+
+		if accessKeyFromEnvExists && secretAccessKeyFromEnvExists {
+			profileName := c.String("profile")
+			profiles, err := cfaws.LoadProfiles()
+			if err != nil {
+				return err
+			}
+
+			if profiles.HasProfile(profileName) {
+				return fmt.Errorf("profile with name '%s' already exist", profileName)
+			}
+
+			// create new static credential
+			credentials, err := credentials.NewStaticCredentialsProvider(accessKeyFromEnv, secretAccessKeyFromEnv, "").Retrieve(ctx)
+			if err != nil {
+				return err
+			}
+
+			secureIAMCredentialStorage := securestorage.NewSecureIAMCredentialStorage()
+			err = secureIAMCredentialStorage.StoreCredentials(profileName, credentials)
+			if err != nil {
+				return err
+			}
+
+			// fetch parsed credentials file
+			credentialsFilePath := cfaws.GetAWSCredentialsPath()
+			credentialsFile, err := ini.LoadSources(ini.LoadOptions{
+				AllowNonUniqueSections:  false,
+				SkipUnrecognizableLines: false,
+			}, credentialsFilePath)
+			if err != nil {
+				return err
+			}
+
+			section, err := credentialsFile.NewSection(profileName)
+			if err != nil {
+				return err
+			}
+			err = section.ReflectFrom(&struct {
+				AWSAccessKeyID     string `ini:"aws_access_key_id"`
+				AWSSecretAccessKey string `ini:"aws_secret_access_key"`
+			}{
+				AWSAccessKeyID:     accessKeyFromEnv,
+				AWSSecretAccessKey: secretAccessKeyFromEnv,
+			})
+			if err != nil {
+				return err
+			}
+			err = credentialsFile.SaveTo(credentialsFilePath)
+			if err != nil {
+				return err
+			}
+
+			err = updateOrCreateProfileWithCredentialProcess(profileName)
+			if err != nil {
+				return err
+			}
+
+			clio.Successf("successfully created new profile %s", profileName)
+
+			return nil
+
+		}
+
+		clio.Error("you don't have variables $AWS_ACCESS_KEY_ID and $AWS_SECRET_ACCESS_KEY set in your environment.")
+		clio.Info("If you instead want to import plain-text credentials from ~/.aws/credentials to secure storage then run 'granted credentials import'")
+
+		return nil
+
 	},
 }
