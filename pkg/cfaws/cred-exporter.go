d3fbf6a58078b7b0480da913d40513d4e71a2043
cedieio <cedric.villanueva@nine.com.au>
2023-12-16 12:22:05 +1000

    🐛 Fix SSO token lookup keys (#565)
    * 🐛 Fix SSO token lookup keys
    
    The lookup key for `GetValidSSOToken` has been updated and the code
    I pushed had the wrong lookup causing panic when trying to export sso
    as it was finding a `nil` token. Updated `GetValidSSOToken` to use
    the combination of `SSOStartURL` and `SSOSessionName` to find it properly.
    Also updated `GetValidSSOTokenFromPlaintextCache` to only use
    `SSOStartURL` as it's the valid value. Should move to use `SSOSessionName`
    in the future. Fix for issue #563.
    
    * Update comment
    
    Updated comment to also include SSOSessionName on the description.

--- pkg/cfaws/cred-exporter.go
+++ pkg/cfaws/cred-exporter.go
@@ -72,8 +72,9 @@ func ExportCredsToProfile(profileName string, creds aws.Credentials) error {
 // ExportAccessTokenToCache will export access tokens to ~/.aws/sso/cache
 func ExportAccessTokenToCache(profile *Profile) error {
 	secureSSOTokenStorage := securestorage.NewSecureSSOTokenStorage()
-	// Find the access token for the SSOStartURL
-	cachedToken := secureSSOTokenStorage.GetValidSSOToken(profile.AWSConfig.SSOStartURL)
+	// Find the access token for the SSOStartURL and SSOSessionName
+	tokenKey := profile.AWSConfig.SSOStartURL + profile.AWSConfig.SSOSessionName
+	cachedToken := secureSSOTokenStorage.GetValidSSOToken(tokenKey)
 	ssoPlainTextOut := CreatePlainTextSSO(profile.AWSConfig, cachedToken)
 	err := ssoPlainTextOut.DumpToCacheDirectory()
 
