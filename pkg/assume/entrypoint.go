aac70b9b299996b68bcb3afd98d29745d7348118
Omar Harvey-Phillips <omar@bymiles.co.uk>
2024-05-22 14:18:22 +0100

    feature/use-aws-user-id-sso: add in an option flag and env flag to optionally call fire GetCallerIdentity and remove the principle id from the user id returned and add that into the roll sessioname

--- pkg/assume/entrypoint.go
+++ pkg/assume/entrypoint.go
@@ -52,6 +52,7 @@ func GlobalFlags() []cli.Flag {
 		&cli.StringFlag{Name: "aws-config-file"},
 		&cli.StringFlag{Name: "chain", Usage: "Assume a given role ARN using the profile selected"},
 		&cli.StringFlag{Name: "reason", Usage: "Provide a reason for requesting access to the role"},
+		&cli.BoolFlag{Name: "use-user-id", Aliases: []string{"uu"}, Usage: "Whether to use the AWS user ID", DefaultText: "false" },
 	}
 }
 
