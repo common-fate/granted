9d9b60fd6b94045563b49065dd156fcef9cf92e9
meyerjrr <jrmeyer339@gmail.com>
2024-06-03 11:16:03 +1000

    update error messages and auto login when grant token is expired

--- pkg/granted/auth/auth.go
+++ pkg/granted/auth/auth.go
@@ -1,7 +1,10 @@
 package auth
 
 import (
+	"strings"
+
 	"github.com/common-fate/cli/cmd/cli/command"
+	"github.com/common-fate/clio"
 	"github.com/common-fate/sdk/config"
 	"github.com/common-fate/sdk/loginflow"
 	"github.com/urfave/cli/v2"
@@ -24,6 +27,15 @@ var loginCommand = cli.Command{
 	Usage: "Authenticate to an OIDC provider",
 	Action: func(c *cli.Context) error {
 		cfg, err := config.LoadDefault(c.Context)
+
+		if err != nil && strings.Contains(err.Error(), "config file does not exist") {
+			clio.Debugw("prompting user login because token is expired", "error_details", err.Error())
+			// NOTE(chrnorm): ideally we'll bubble up a more strongly typed error in future here, to avoid the string comparison on the error message.
+
+			// the OAuth2.0 token is expired so we should prompt the user to log in
+			clio.Infof("Config file not found. To get set up with Common Fate run `granted auth configure https://cf.demo.io`")
+
+		}
 		if err != nil {
 			return err
 		}
