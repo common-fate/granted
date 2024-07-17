526a8abd92c84328a717bf6966b2b53a55b64fb2
ckluy31 <calvinluy@gmail.com>
2024-07-17 14:59:20 +1000

    Sanity check error matching of invalid_grant

--- pkg/hook/accessrequesthook/accessrequesthook.go
+++ pkg/hook/accessrequesthook/accessrequesthook.go
@@ -420,6 +420,13 @@ func shouldRefreshLogin(err error) bool {
 	if strings.Contains(err.Error(), "oauth2: invalid grant") {
 		return true
 	}
+	// Sanity check that error message is matching correctly. See: https://linear.app/common-fate/issue/CF-3379/granted-should-auto-login-when-receiving-an-invalid-grant-error
+	if strings.Contains(err.Error(), `oauth2: "token_expired"`) {
+		return true
+	}
+	if strings.Contains(err.Error(), `oauth2: "invalid_grant"`) {
+		return true
+	}
 
 	return false
 }
