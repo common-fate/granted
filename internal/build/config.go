ae6fb6b11d907f0d5da6897d689d4c768de6f3e0
chrnorm <17420369+chrnorm@users.noreply.github.com>
2024-09-30 20:33:53 +0100

    update extension ID

--- internal/build/config.go
+++ internal/build/config.go
@@ -3,5 +3,5 @@ package build
 // these configs are overridden as part of the release build process.
 var (
 	ConfigFolderName  = ".dgranted"
-	ChromeExtensionID = "fcipjekpmlpmiikgdecbjbcpmenmceoh"
+	ChromeExtensionID = "cjjieeldgoohbkifkogalkmfpddeafcm"
 )
