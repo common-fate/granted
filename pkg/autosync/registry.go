dcf0135a52adce8cc12d8bfb84ddc38b53fb6bed
Eloy Tolosa <etolosa@proadata.com>
2024-11-15 10:41:51 +0100

    removed commented and unused code

--- pkg/autosync/registry.go
+++ pkg/autosync/registry.go
@@ -9,25 +9,6 @@ import (
 	"github.com/common-fate/granted/pkg/granted/registry"
 )
 
-// This error is not needed, as it is not being used anywhere and
-// does not offer us an specific behaviour on the event of the error
-// occurring, so we will just eliminate it
-//
-// type RegistrySyncError struct {
-// 	err error
-// }
-
-// func (e *RegistrySyncError) Error() string {
-// 	return fmt.Sprintf("error syncing profile registry with err: %s", e.err.Error())
-// }
-// type RegistrySyncError struct {
-// 	err error
-// }
-
-// func (e *RegistrySyncError) Error() string {
-// 	return fmt.Sprintf("error syncing profile registry with err: %s", e.err.Error())
-// }
-
 func runSync(ctx context.Context, rc RegistrySyncConfig, interactive bool) error {
 	clio.Info("Syncing Profile Registries")
 	err := registry.SyncProfileRegistries(ctx, interactive)
