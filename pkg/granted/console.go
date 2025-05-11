6b84727f77bc78f6952cd74354aa5ec4d64022c1
Chris Norman <17420369+chrnorm@users.noreply.github.com>
2025-05-11 16:22:01 +0100

    Revert "Added ability to specify multiple services top be opened with the --c…" (#872)
    This reverts commit 479cc4aeb856ce5207b7dee3cbbd16346dea510e.

--- pkg/granted/console.go
+++ pkg/granted/console.go
@@ -40,107 +40,103 @@ var ConsoleCommand = cli.Command{
 			return err
 		}
 		con := console.AWS{
-			Service:     []string{c.String("service")},
+			Service:     c.String("service"),
 			Region:      c.String("region"),
 			Destination: c.String("destination"),
 		}
 
-		consoleURLs, err := con.URLs(*credentials)
+		consoleURL, err := con.URL(*credentials)
 		if err != nil {
 			return err
 		}
 
-		for _, consoleURL := range consoleURLs {
+		cfg, err := config.Load()
+		if err != nil {
+			return err
+		}
+		if c.Bool("firefox") || cfg.DefaultBrowser == browser.FirefoxKey || cfg.DefaultBrowser == browser.FirefoxStdoutKey {
+			// transform the URL into the Firefox Tab Container format.
+			consoleURL = fmt.Sprintf("ext+granted-containers:name=%s&url=%s&color=%s&icon=%s", c.String("container-name"), url.QueryEscape(consoleURL), c.String("color"), c.String("icon"))
+		}
 
-			cfg, err := config.Load()
-			if err != nil {
-				return err
-			}
-			if c.Bool("firefox") || cfg.DefaultBrowser == browser.FirefoxKey || cfg.DefaultBrowser == browser.FirefoxStdoutKey {
-				// transform the URL into the Firefox Tab Container format.
-				consoleURL = fmt.Sprintf("ext+granted-containers:name=%s&url=%s&color=%s&icon=%s", c.String("container-name"), url.QueryEscape(consoleURL), c.String("color"), c.String("icon"))
-			}
+		justPrintURL := c.Bool("url") || cfg.DefaultBrowser == browser.StdoutKey || cfg.DefaultBrowser == browser.FirefoxStdoutKey
+		if justPrintURL {
+			// return the url via stdout through the CLI wrapper script and return early.
+			fmt.Print(consoleURL)
+			return nil
+		}
 
-			justPrintURL := c.Bool("url") || cfg.DefaultBrowser == browser.StdoutKey || cfg.DefaultBrowser == browser.FirefoxStdoutKey
-			if justPrintURL {
-				// return the url via stdout through the CLI wrapper script and return early.
-				fmt.Print(consoleURL)
-				return nil
-			}
-
-			var l assume.Launcher
-			if cfg.CustomBrowserPath == "" && cfg.DefaultBrowser != "" {
-				l = launcher.Open{}
-			} else if cfg.CustomBrowserPath == "" && cfg.AWSConsoleBrowserLaunchTemplate == nil {
-				return errors.New("default browser not configured. run `granted browser set` to configure")
-			} else {
-				switch cfg.DefaultBrowser {
-				case browser.ChromeKey:
-					l = launcher.ChromeProfile{
-						ExecutablePath: cfg.CustomBrowserPath,
-					}
-				case browser.BraveKey:
-					l = launcher.ChromeProfile{
-						ExecutablePath: cfg.CustomBrowserPath,
-					}
-				case browser.EdgeKey:
-					l = launcher.ChromeProfile{
-						ExecutablePath: cfg.CustomBrowserPath,
-					}
-				case browser.ChromiumKey:
-					l = launcher.ChromeProfile{
-						ExecutablePath: cfg.CustomBrowserPath,
-					}
-				case browser.VivaldiKey:
-					l = launcher.ChromeProfile{
-						ExecutablePath: cfg.CustomBrowserPath,
-					}
-				case browser.FirefoxKey:
-					l = launcher.Firefox{
-						ExecutablePath: cfg.CustomBrowserPath,
-					}
-				case browser.SafariKey:
-					l = launcher.Safari{}
-				case browser.CustomKey:
-					l, err = launcher.CustomFromLaunchTemplate(cfg.AWSConsoleBrowserLaunchTemplate, c.StringSlice("browser-launch-template-arg"))
-					if err == launcher.ErrLaunchTemplateNotConfigured {
-						return errors.New("error configuring custom browser, ensure that [AWSConsoleBrowserLaunchTemplate] is specified in your Granted config file")
-					}
-					if err != nil {
-						return err
-					}
-				default:
-					l = launcher.Open{}
+		var l assume.Launcher
+		if cfg.CustomBrowserPath == "" && cfg.DefaultBrowser != "" {
+			l = launcher.Open{}
+		} else if cfg.CustomBrowserPath == "" && cfg.AWSConsoleBrowserLaunchTemplate == nil {
+			return errors.New("default browser not configured. run `granted browser set` to configure")
+		} else {
+			switch cfg.DefaultBrowser {
+			case browser.ChromeKey:
+				l = launcher.ChromeProfile{
+					ExecutablePath: cfg.CustomBrowserPath,
 				}
-			}
-			// now build the actual command to run - e.g. 'firefox --new-tab <URL>'
-			args, err := l.LaunchCommand(consoleURL, con.Profile)
-			if err != nil {
-				return fmt.Errorf("error building browser launch command: %w", err)
-			}
-
-			var startErr error
-			if l.UseForkProcess() {
-				clio.Debugf("running command using forkprocess: %s", args)
-				cmd, err := forkprocess.New(args...)
+			case browser.BraveKey:
+				l = launcher.ChromeProfile{
+					ExecutablePath: cfg.CustomBrowserPath,
+				}
+			case browser.EdgeKey:
+				l = launcher.ChromeProfile{
+					ExecutablePath: cfg.CustomBrowserPath,
+				}
+			case browser.ChromiumKey:
+				l = launcher.ChromeProfile{
+					ExecutablePath: cfg.CustomBrowserPath,
+				}
+			case browser.VivaldiKey:
+				l = launcher.ChromeProfile{
+					ExecutablePath: cfg.CustomBrowserPath,
+				}
+			case browser.FirefoxKey:
+				l = launcher.Firefox{
+					ExecutablePath: cfg.CustomBrowserPath,
+				}
+			case browser.SafariKey:
+				l = launcher.Safari{}
+			case browser.CustomKey:
+				l, err = launcher.CustomFromLaunchTemplate(cfg.AWSConsoleBrowserLaunchTemplate, c.StringSlice("browser-launch-template-arg"))
+				if err == launcher.ErrLaunchTemplateNotConfigured {
+					return errors.New("error configuring custom browser, ensure that [AWSConsoleBrowserLaunchTemplate] is specified in your Granted config file")
+				}
 				if err != nil {
 					return err
 				}
-				startErr = cmd.Start()
-			} else {
-				clio.Debugf("running command without forkprocess: %s", args)
-				cmd := exec.Command(args[0], args[1:]...)
-				startErr = cmd.Start()
+			default:
+				l = launcher.Open{}
 			}
+		}
+		// now build the actual command to run - e.g. 'firefox --new-tab <URL>'
+		args, err := l.LaunchCommand(consoleURL, con.Profile)
+		if err != nil {
+			return fmt.Errorf("error building browser launch command: %w", err)
+		}
 
-			if startErr != nil {
-				return clierr.New(fmt.Sprintf("Granted was unable to open a browser session automatically due to the following error: %s", startErr.Error()),
-					// allow them to try open the url manually
-					clierr.Info("You can open the browser session manually using the following url:"),
-					clierr.Info(consoleURL),
-				)
+		var startErr error
+		if l.UseForkProcess() {
+			clio.Debugf("running command using forkprocess: %s", args)
+			cmd, err := forkprocess.New(args...)
+			if err != nil {
+				return err
 			}
-			return nil
+			startErr = cmd.Start()
+		} else {
+			clio.Debugf("running command without forkprocess: %s", args)
+			cmd := exec.Command(args[0], args[1:]...)
+			startErr = cmd.Start()
+		}
+
+		if startErr != nil {
+			return clierr.New(fmt.Sprintf("Granted was unable to open a browser session automatically due to the following error: %s", startErr.Error()),
+				// allow them to try open the url manually
+				clierr.Info("You can open the browser session manually using the following url:"),
+				clierr.Info(consoleURL),
+			)
 		}
 		return nil
 	},
